74f9735de4f26dca210585ed471770e05ae6875c
Tống Phan Kim Thạch <22637901.thach@student.iuh.edu.vn>
2025-04-04 20:28:10 +0700

    First Command

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+   
+    <div class="container">
+  <div class="header"><h2>My Header</h2></div>
+  <div class="menu"></div>
+  <div class="content"><h3>thach dep trai</h3><p>nay la phan noi dung</p></div>
+  <div class="footer"><h4>Footer</h4></div>
+    </div>
+  )
+}
+
+export default App
